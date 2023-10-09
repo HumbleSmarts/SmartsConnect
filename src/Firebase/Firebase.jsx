b14abc5f984d73f7060b6c9700f538f33b22d540
humblesmarts <humblesmarts11@gmail.com>
2023-10-09 09:06:16 +0100

    first commit

--- src/Firebase/Firebase.jsx
+++ src/Firebase/Firebase.jsx
@@ -9,11 +9,12 @@ const provider = new firebase.auth.GoogleAuthProvider();
 const firebaseConfig = {
   apiKey: "AIzaSyCB0n94KoStejHlesi2ZGov5maX9KGZrp0",
   authDomain: "smarts-connect.firebaseapp.com",
+  databaseURL: "https://smarts-connect-default-rtdb.firebaseio.com",
   projectId: "smarts-connect",
   storageBucket: "smarts-connect.appspot.com",
   messagingSenderId: "540898223824",
-  appId: "1:540898223824:web:f5a6dfe86916ea363660d5",
-  measurementId: "G-2YWGHZBPXC",
+  appId: "1:540898223824:web:29f1276af3d310773660d5",
+  measurementId: "G-8MEQHXRRB2",
 };
 
 const app = initializeApp(firebaseConfig);
